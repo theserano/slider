22b0e2761630b3ff893a95599157b5dac1e0b368
Zoe Serano <edwardserano@gmail.com>
2024-06-29 10:14:36 +0100

    prettier formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css';
-import { useRef, useState } from 'react';
+import "./App.css";
+import { useRef, useState } from "react";
 import audi from "./assets/audi.jpeg";
 import lambo from "./assets/aventador.jpeg";
 import jeep from "./assets/jeep defender.jpeg";
 import alpha from "./assets/alpha.jpeg";
 import rolls from "./assets/rolls.jpeg";
 import { IoChevronForwardOutline, IoChevronBackOutline } from "react-icons/io5";
-import Container from './components/Container';
-
+import Container from "./components/Container";
 
 function App() {
   const [activeTab, setActiveTab] = useState(1);
@@ -17,57 +16,72 @@ function App() {
 
   const handleSlide = (direction: string) => {
     let newTab;
-    if (direction === 'forward') {
+    if (direction === "forward") {
       newTab = activeTab === tabs.length ? 1 : activeTab + 1;
     } else {
       newTab = activeTab === 1 ? tabs.length : activeTab - 1;
     }
     setActiveTab(newTab);
-  }
+  };
 
   return (
-    <div className='app'>
-      <p>Please open on a larger screen.<br /> (preferably a laptop)</p>
+    <div className="app">
+      <p>
+        Please open on a larger screen.
+        <br /> (preferably a laptop)
+      </p>
       {activeTab === 1 && (
         <Container
-          header='Audi R8'
-          text='The Audi R8 embodies elegance and speed, offering a thrilling driving experience.'
+          header="Audi R8"
+          text="The Audi R8 embodies elegance and speed, offering a thrilling driving experience."
           image={audi}
         />
       )}
-      
+
       {activeTab === 2 && (
         <Container
-          header='Lamborghini Aventador'
-          text='The Lamborghini Aventador is an automotive masterpiece, blending exquisite design with blistering performance. '
+          header="Lamborghini Aventador"
+          text="The Lamborghini Aventador is an automotive masterpiece, blending exquisite design with blistering performance. "
           image={lambo}
         />
       )}
-      
+
       {activeTab === 3 && (
         <Container
-          header='Jeep Defender'
-          text='The Jeep Defender, a rugged off-road marvel, embodies adventure and durability in its iconic design.'
+          header="Jeep Defender"
+          text="The Jeep Defender, a rugged off-road marvel, embodies adventure and durability in its iconic design."
           image={jeep}
         />
       )}
       {activeTab === 4 && (
         <Container
-          header='Alpha Romeo 4C'
-          text='It is a two-seater, mid-engined sport car. It is a two-seater, mid-engined sports car.'
+          header="Alpha Romeo 4C"
+          text="It is a two-seater, mid-engined sport car. It is a two-seater, mid-engined sports car."
           image={alpha}
         />
       )}
       {activeTab === 5 && (
         <Container
-          header='Rolls-Royce Ghost'
-          text='This is a luxury sedan known for its spacious interior, powerful engine, and smooth ride.'
+          header="Rolls-Royce Ghost"
+          text="This is a luxury sedan known for its spacious interior, powerful engine, and smooth ride."
           image={rolls}
         />
       )}
       <div className="btn_group">
-        <span ref={backwardRef} onClick={() => handleSlide('backward')} className='one'><IoChevronBackOutline /></span>
-        <span ref={forwardRef} onClick={() => handleSlide('forward')} className='two'><IoChevronForwardOutline /></span>
+        <span
+          ref={backwardRef}
+          onClick={() => handleSlide("backward")}
+          className="one"
+        >
+          <IoChevronBackOutline />
+        </span>
+        <span
+          ref={forwardRef}
+          onClick={() => handleSlide("forward")}
+          className="two"
+        >
+          <IoChevronForwardOutline />
+        </span>
       </div>
     </div>
   );
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,32 +1,34 @@
-import './container.css';
+import "./container.css";
 import { motion } from "framer-motion";
 
-
 interface ContainerProps {
-    header: string;
-    text: string;
-    image: string;
+  header: string;
+  text: string;
+  image: string;
 }
 
-const Container = ({ header, text, image}: ContainerProps) => {
-    return (
-        <motion.div
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1.01 }}
-            exit={{ opacity: 0, scale: 0.2 }}
-            transition={{ duration: 2 }}
-            className="container"
-        >
-            <div className={`container_image}`}>
-                <img src={image} alt="image" />
-            </div>
+const Container = ({ header, text, image }: ContainerProps) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1.01 }}
+      exit={{ opacity: 0, scale: 0.2 }}
+      transition={{ duration: 2 }}
+      className="container"
+    >
+      <div className={`container_image}`}>
+        <img src={image} alt="image" />
+      </div>
 
-            <div className="container_box">
-                <div className={`container_header`}>{header}</div>
-                <div className={`container_text`}><span>* </span>{text}</div>
-            </div>
-        </motion.div>
-    );
-}
+      <div className="container_box">
+        <div className={`container_header`}>{header}</div>
+        <div className={`container_text`}>
+          <span>* </span>
+          {text}
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default Container;
